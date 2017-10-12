63fe782e908b2588ab8a7ea07608789eaae936cd
BOCCARD Gregory <Gregory.BOCCARD@efgbank.com>
2017-10-12 23:21:27 +0200

    Refactor Test

--- dapp/RaffleDraw-v2/contracts/RaffleDraw.sol
+++ dapp/RaffleDraw-v2/contracts/RaffleDraw.sol
@@ -15,85 +15,106 @@ contract RaffleDraw {
     address internal owner;
     string public gameName;
 
-    uint public winningNumber;
-
-
 
     function RaffleDraw() {
         owner = msg.sender;
         gameName = "RaffleDraw 1.0";
     }
     
-    function AddUser(string name) public onlyByAdmin() {
+    function AddUser(string name) onlyByAdmin() {
         users.push(Users(name, "", false, false));
     }
 
-    function NbUsers() returns(uint) {
+    function NbUsers() constant returns(uint) {
         return users.length;
     }
 
-    function AddPrize(string prize) public onlyByAdmin() {
+    function AddPrize(string prize) onlyByAdmin() {
         prizes.push(prize);
-    } 
+    }
 
-    function RemovePrize(uint index) public onlyByAdmin() {
+    function RemovePrize(uint index) onlyByAdmin() {
         delete prizes[index];
         prizes[index]=prizes[prizes.length-1];
         prizes.length --;
     }
 
-    function NbPrizes() returns(uint) {
+    function NbPrizes() constant returns(uint) {
         return prizes.length;
-    }
-
-
-    event TestRandomEvent(uint random, bool returnValue);
-
-    function TestRandom() public returns(bool) {
-        uint random = uint(block.blockhash(block.number-1)) % users.length;
-        TestRandomEvent(random, true);
-        return true;
     }
 
     event DrawEvent(uint randomU, uint randomP, string winner, string prize);
 
     // Will get user and prize index, followed by winner name and prize description
-    function Draw() public onlyByAdmin() returns(uint, uint, string, string) {
+    function Draw() onlyByAdmin() returns(bool) {
+
+
+        // ####################### test edge cases!!!
+        // exit if no user or no prize
+        require(users.length > 0);
+        require(prizes.length > 0);
+
         uint randomU = uint(block.blockhash(block.number-1)) % users.length;
-        //uint randomU = 1;
         var winner = users[randomU].name;
         users[randomU].drawn = true;
 
         uint randomP = uint(block.blockhash(block.number-1)) % prizes.length;
         var prize = prizes[randomP];
-        return (randomU, randomP, winner, prize);
+
+        DrawEvent(randomU, randomP, winner, prize);
+        return true;
     }
 
-    //Given last draw (user+prize index), we will get a new winner index+name, with same prize
-    function ReDrawUser(uint indexU, uint indexP) public  onlyByAdmin() returns(uint, string) {
-        uint newRandomN = uint(block.blockhash(block.number-1)) % users.length;
+    event ReDrawUserEvent(uint newRandomU, string newWinner);
+
+    //Given last draw (user&prize index), we will get a new winner index and name, we are not touching the prize
+    function ReDrawUser(uint indexU, uint indexP) onlyByAdmin() returns(bool) {
+
+        require(users.length > 1); // need at minimum 2 users to swap each other
+        require(indexP < prizes.length && prizes.length > 0); // a prize should exit
+
+        uint newRandomU = uint(block.blockhash(block.number-1)) % users.length;
         // exit if only 1 user left
-        //if (users.length == 1) { return; }
+        if (users.length == 1) { return; }
         // choose a different user than the one we want to replace
-        if (newRandomN == indexU) { newRandomN = newRandomN + 1; }
+        if (newRandomU == indexU) { newRandomU = newRandomU + 1; }
         // if we are out of the table, choose the preceding user
-        if (newRandomN > users.length) { newRandomN = newRandomN - 1; }
+        if (newRandomU > users.length) { newRandomU = newRandomU - 1; }
         
-        var newWinner = users[newRandomN].name;
-        //users[newRandomN].drawn = true;
+        var newWinner = users[newRandomU].name;
+        //users[newRandomU].drawn = true;
         users[indexU].drawn = false; // reset previous winner
-        return (newRandomN, newWinner);
+
+        ReDrawUserEvent(newRandomU, newWinner);
+        return true;
     }
 
-    function AcceptDraw(uint indexU, uint indexP) public onlyByAdmin() {
+    event AcceptDrawEvent(string winner, string prize);
+
+    function AcceptDraw(uint indexU, uint indexP) onlyByAdmin() returns(bool) {
+        var prize = prizes[indexP];
         users[indexU].prize = prizes[indexP];
+        
+        var winner = users[indexU].name;
         users[indexU].accepted = true;
+
+        AcceptDrawEvent(winner, prize);
+
         RemovePrize(indexP);
+        return true;
     }
 
     modifier onlyByAdmin() {
         require(msg.sender == owner);
         _;
     }
+
+    // event TestRandomEvent(uint random, bool returnValue);
+
+    // function TestRandom() public returns(bool) {
+    //     uint random = uint(block.blockhash(block.number-1)) % users.length;
+    //     TestRandomEvent(random, true);
+    //     return true;
+    // }
 
 }
--- dapp/RaffleDraw-v2/test/raffleDraw.js
+++ dapp/RaffleDraw-v2/test/raffleDraw.js
@@ -1,97 +1,220 @@
 //const Web3 = require('web3')
 var RaffleDraw = artifacts.require("./RaffleDraw.sol");
 
-contract('RaffleDraw', function(accounts) {
-
-  // Deploy the app and put the returning object in var contract
-  var contract;
-  RaffleDraw.deployed().then(function(instance) {
-      contract = instance;
-  });
-
+contract('RaffleDraw: Simple Checks', function(accounts) {
   var bob = accounts[0];
   var alice = accounts[1];
 
-  it("will return the deployment contract vars", function() {
-      //console.log(contract);    <-- display all var of the contract
-      sender=contract.constructor.class_defaults.from;
-      balance_sender = web3.fromWei(web3.eth.getBalance(sender).toNumber(),'ether');
-      console.log("Sender/owner: addr=" + sender + " -> balance=" + balance_sender);
-
-      balance = web3.fromWei(web3.eth.getBalance(contract.address).toNumber(),'ether'); 
-      console.log("Contract: addr=" + contract.address + " -> balance=" + balance);
-      //console.log("ABI:");
-      //console.log(contract.abi);
-  });
-
-  it("will return the in-app vars", function() {
-      return contract.gameName().then( gameName => { console.log("Game name: " + gameName) });
-  });
-
-  it("will check alice(not admin) can't add users ", function() {
-       contract.AddUser("Vince", {from:alice}).catch ( error => { assert.equal(error.message, "VM Exception while processing transaction: invalid opcode", "Alice should not be able to add users") }); 
-  });
-
-  it("will check Bob(admin) can add users", function() {
-      contract.AddUser("Anne", {from:bob}); 
-      contract.AddUser("Bart", {from:bob});
-      contract.AddUser("Carl", {from:bob});
-      //contract.AddUser("Dom", {from:bob});  
-      return contract.users(2).then ( name => { assert.equal(name[0], "Carl", "Bob should be able to add users") }); 
-  });
-
-  it("will check Bob(admin) can add prizes", function() {
-      contract.AddPrize("OneBitcoin", {from:bob});
-      contract.AddPrize("TwoEthereum", {from:bob});
-      return contract.prizes(1).then ( prize => { assert.equal(prize, "TwoEthereum", "Bob should be able to add prizes") }); 
-  });
-
-  it("will list names and prizes", function() {
-      contract.NbUsers.call().then( nbUsers => { console.log("NbUsers:"+ nbUsers) });
-      console.log("User,prize,drawn,accepted");
-      contract.users.call(0).then( user => { console.log("- User[0]:"+ user) });
-      contract.users.call(1).then( user => { console.log("- User[1]:"+ user) });
-      contract.users.call(2).then( user => { console.log("- User[2]:"+ user) });
-      contract.NbPrizes.call().then( nbPrizes => { console.log("NbPrizes:"+ nbPrizes) });
-      contract.prizes.call(0).then( prize => { console.log("- Prize[0]:"+ prize) });
-      return contract.prizes.call(1).then( prize => { console.log("- Prize[1]:"+ prize) });
-  });
-
-  it("will test random number as an event", function() {
-      contract.TestRandom().then( tx => {
-  
-          var event = contract.TestRandomEvent({fromBlock: 0, toBlock: 'latest'});
-          event.watch(function(error, result){
-              if (!error)
-                  var r = result[0];
-                  console.log(result.args.random.valueOf());
-                  console.log(result.args.returnValue);
-                  event.stopWatching();
+  it("will check bob(admin) can add users", function(done) {
+      RaffleDraw.deployed()
+          .then( instance => { instance.AddUser("Anne", {from:bob}); return instance; })
+          .then( instance => { instance.AddUser("Bart", {from:bob}); return instance; })
+          .then( instance => { instance.AddUser("Carl", {from:bob}); return instance; })
+          .then( instance => { instance.AddUser("Dom", {from:bob}); return instance; })
+          .then( instance => { return instance.users(3); })
+          .then( user => {
+              assert.equal(user[0], "Dom", "Bob should be able to add users");
+              done();
           });
-      }); 
   });
 
-  // it("will draw winner and price, redraw again for another name, and accept prize ", function() {
-  //     return contract.Draw().then( tx => {
-  //         return contract.Draw.call().then( winner => { console.log("--> Winner(indexU,indexP,user,price) : "+ winner); 
-  //             return contract.ReDrawUser(winner[0],winner[1]).then( tx => {
-  //                 return contract.ReDrawUser.call(winner[0],winner[1]).then( newwinner => { console.log("--> New winner instead:"+ newwinner);
-  //                     return contract.AcceptDraw(newwinner[0],winner[1]).then( tx => {
-  //                         return contract.NbPrizes.call().then( nbPrizes => { 
-  //                             console.log("NbPrize left:"+ nbPrizes);
-  //                             contract.users.call(newwinner[0]).then( user => { console.log("- Check winner :"+ user) });
-  //                             contract.users.call(0).then( user => { console.log("- User[0] :"+ user) });
-  //                             contract.users.call(1).then( user => { console.log("- User[1] :"+ user) });
-  //                             return contract.users.call(2).then( user => { console.log("- User[2] :"+ user) }); 
+  it("will check bob(admin) can add prizes", function(done) {
+      RaffleDraw.deployed()
+          .then( instance => { instance.AddPrize("OneBitcoin", {from:bob}); return instance; })
+          .then( instance => { instance.AddPrize("TwoEthereum", {from:bob}); return instance; })
+          .then( instance => { return instance.prizes(1); })
+          .then( prize => {
+              assert.equal(prize, "TwoEthereum", "Bob should be able to add prizes");
+              done();
+          });
+  });
 
+});
+
+
+contract('RaffleDraw: reject actions', function(accounts) {
+  var bob = accounts[0];
+  var alice = accounts[1];
+
+  it("will check alice(not admin) can't add users", function(done) {
+      RaffleDraw.deployed()
+          .then( instance => { instance.AddUser("Anne", {from:alice})
+              .catch ( error => { assert.equal(error.message, "VM Exception while processing transaction: invalid opcode", "Alice should not be able to add users") })
+              done();
+          });
+  });
+
+  it("will check bob can't draw if not user registered", function(done) {
+      RaffleDraw.deployed()
+          .then( instance => { instance.Draw({from:bob})
+              .catch ( error => { assert.equal(error.message, "VM Exception while processing transaction: invalid opcode", "Alice should not be able to add users") })
+              done();
+          });
+  });
+
+
+});
+
+
+
+// contract('RaffleDraw: full raffle workflow', function(accounts) {
+//   var bob = accounts[0];
+//   var alice = accounts[1];
+
+//   it("will draw winner&price, redraw again for another winner, and accept winner&prize ", function(done) {
+//       RaffleDraw.deployed()
+//           .then( instance => { instance.AddUser("Anne", {from:bob}); return instance; })
+//           .then( instance => { instance.AddUser("Bart", {from:bob}); return instance; })
+//           .then( instance => { instance.AddUser("Carl", {from:bob}); return instance; })
+//           .then( instance => { instance.AddUser("Dom", {from:bob}); return instance; })
+//           .then( instance => { instance.AddPrize("OneBitcoin", {from:bob}); return instance; })
+//           .then( instance => { instance.AddPrize("TwoEthereum", {from:bob}); return instance; })
+//           .then( instance => { instance.Draw() ; return instance; })
+//           .then( instance => {
+//               var event = instance.DrawEvent({fromBlock: 0, toBlock: 'latest'});
+//               event.watch(function(error, result){
+//                   if (!error)
+//                   var indexU = result.args.randomU;
+//                   var indexP = result.args.randomP;
+//                   console.log("DrawEvent => (indexU, indexP, winner, prize) => " + indexU + "," + indexP + "," + result.args.winner + "," + result.args.prize);
+//                   event.stopWatching();
+              
+//                   instance.ReDrawUser(indexU,indexP)
+//                   .then ( tx => {
+//                       var event2 = instance.ReDrawUserEvent({fromBlock: 0, toBlock: 'latest'});
+//                       event2.watch(function(error, result2){
+//                           if (!error)
+//                           var newIndexU = result2.args.newRandomU;
+//                           console.log("ReDrawUserEvent => (newIndexU, newWinner) => " + newIndexU + "," + result2.args.newWinner );
+//                           event2.stopWatching();
+
+//                           instance.AcceptDraw(newIndexU,indexP)
+//                           .then( tx => {
+//                               var event3 = instance.AcceptDrawEvent({fromBlock: 0, toBlock: 'latest'});
+//                               event3.watch(function(error, result3){
+//                                   if (!error)
+//                                   console.log("AcceptDrawEvent => (finalWinner, prize) => " + result3.args.winner + "," + result3.args.prize );
+//                                   event3.stopWatching();
+//                               });
+//                           });
+//                       });
+//                   });
+//               });
+//           done();
+//           });
+//   });
+
+
+// });
+
+
+
+
+
+
+  // var contract_;
+  // RaffleDraw.deployed().then(function(instance) {
+  //     contract_ = instance;
+  // });
+
+  // // Deploy the app and put the returning object in var contract
+  // var contract;
+  // RaffleDraw.deployed().then(function(instance) {
+  //     contract = instance;
+  // });
+
+  // it("will check Bob(admin) can add users", function() {
+  //     contract.AddUser("Anne", {from:bob}); 
+  //     contract.AddUser("Bart", {from:bob});
+  //     contract.AddUser("Carl", {from:bob});
+  //     //contract.AddUser("Dom", {from:bob});  
+  //     return contract.users(2).then ( name => { assert.equal(name[0], "Carl", "Bob should be able to add users") }); 
+  // });
+
+  // it("will check Bob(admin) can add prizes", function() {
+  //     contract.AddPrize("OneBitcoin", {from:bob});
+  //     contract.AddPrize("TwoEthereum", {from:bob});
+  //     return contract.prizes(1).then ( prize => { assert.equal(prize, "TwoEthereum", "Bob should be able to add prizes") }); 
+  // });
+
+  // it("will list names and prizes", function() {
+  //     contract.NbUsers.call().then( nbUsers => { console.log("NbUsers:"+ nbUsers) });
+  //     console.log("User,prize,drawn,accepted");
+  //     contract.users.call(0).then( user => { console.log("- User[0]:"+ user) });
+  //     contract.users.call(1).then( user => { console.log("- User[1]:"+ user) });
+  //     contract.users.call(2).then( user => { console.log("- User[2]:"+ user) });
+  //     contract.NbPrizes.call().then( nbPrizes => { console.log("NbPrizes:"+ nbPrizes) });
+  //     contract.prizes.call(0).then( prize => { console.log("- Prize[0]:"+ prize) });
+  //     return contract.prizes.call(1).then( prize => { console.log("- Prize[1]:"+ prize) });
+  // });
+
+  // it("will draw winner&price, redraw again for another winner, and accept winner&prize ", function() {
+  //     contract.Draw().then( tx => {
+
+  //         var event = contract.DrawEvent({fromBlock: 0, toBlock: 'latest'});
+  //         event.watch(function(error, result){
+  //             if (!error)
+  //             var indexU = result.args.randomU;
+  //             var indexP = result.args.randomP;
+  //             console.log("DrawEvent => (indexU, indexP, winner, prize) => " + indexU + "," + indexP + "," + result.args.winner + "," + result.args.prize);
+  //             event.stopWatching();
+              
+  //             contract.ReDrawUser(indexU,indexP).then( tx => {
+
+  //                 var event2 = contract.ReDrawUserEvent({fromBlock: 0, toBlock: 'latest'});
+  //                 event2.watch(function(error, result2){
+  //                     if (!error)
+  //                     var newIndexU = result2.args.newRandomU;
+  //                     console.log("ReDrawUserEvent => (newIndexU, newWinner) => " + newIndexU + "," + result2.args.newWinner );
+  //                     event2.stopWatching();
+                      
+  //                     contract.AcceptDraw(newIndexU,indexP).then( tx => {
+  //                         var event3 = contract.AcceptDrawEvent({fromBlock: 0, toBlock: 'latest'});
+  //                         event3.watch(function(error, result3){
+  //                             if (!error)
+  //                             console.log("AcceptDrawEvent => (finalWinner, prize) => " + result3.args.winner + "," + result3.args.prize );
+  //                             event3.stopWatching();
   //                         });
   //                     });
+
   //                 });
   //             });
+
   //         });
   //     });
   // });
 
+
+
+
+
+
+//----------------------
+  // it("will redraw winner", function() {
+  //     contract.ReDrawUser(0,0).then( tx => {
+
+  //         var event = contract.ReDrawUserEvent({fromBlock: 0, toBlock: 'latest'});
+  //         event.watch(function(error, result2){
+  //             if (!error)
+  //             console.log("ReDrawUserEvent: " + result2.args.newRandomU + "," + result2.args.newWinner );
+  //             event.stopWatching();
+  //         });
+  //     });
+  // });
+
+
+  // it("will test random number as an event", function() {
+  //     contract.TestRandom().then( tx => {
+  
+  //         var event = contract.TestRandomEvent({fromBlock: 0, toBlock: 'latest'});
+  //         event.watch(function(error, result){
+  //             if (!error)
+  //                 console.log(result.args.random.valueOf());
+  //                 console.log(result.args.returnValue);
+  //                 event.stopWatching();
+  //         });
+  //     }); 
+  // });
 
 
   // it("will draw price and winner ", function() {
@@ -110,7 +233,7 @@ contract('RaffleDraw', function(accounts) {
   //     });
   // });
 
-});
+
 
 
 
