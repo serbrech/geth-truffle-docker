da9b70d698734cb1284edb7551aac02528ebc386
BOCCARD Gregory <Gregory.BOCCARD@efgbank.com>
2017-10-13 21:25:20 +0200

    Add more test cases

--- dapp/RaffleDraw-v2/contracts/RaffleDraw.sol
+++ dapp/RaffleDraw-v2/contracts/RaffleDraw.sol
@@ -48,11 +48,9 @@ contract RaffleDraw {
     // Will get user and prize index, followed by winner name and prize description
     function Draw() onlyByAdmin() returns(bool) {
 
-
-        // ####################### test edge cases!!!
         // exit if no user or no prize
-        require(users.length > 0);
-        require(prizes.length > 0);
+        require(users.length > 0); // need at least 1 user to draw
+        require(prizes.length > 0); // need at least 1 prize to draw
 
         uint randomU = uint(block.blockhash(block.number-1)) % users.length;
         var winner = users[randomU].name;
@@ -60,31 +58,34 @@ contract RaffleDraw {
 
         uint randomP = uint(block.blockhash(block.number-1)) % prizes.length;
         var prize = prizes[randomP];
-
+        users[randomU].prize = prize;
+        
         DrawEvent(randomU, randomP, winner, prize);
         return true;
     }
+
 
     event ReDrawUserEvent(uint newRandomU, string newWinner);
 
     //Given last draw (user&prize index), we will get a new winner index and name, we are not touching the prize
     function ReDrawUser(uint indexU, uint indexP) onlyByAdmin() returns(bool) {
 
-        require(users.length > 1); // need at minimum 2 users to swap each other
-        require(indexP < prizes.length && prizes.length > 0); // a prize should exit
+        require(users.length > 1); // need at least 2 users to swap each other
+        require(indexP < prizes.length && prizes.length > 0); // a prize should exist and inside the array range
+        require(users[indexU].drawn == true); // user need to have been drawn to be able to be swapped
+        require( sha3(users[indexU].prize) == sha3(prizes[indexP]) ); // User to swap should have been drawn with the related prize -> sha3: can only compare int/bool in require()
 
         uint newRandomU = uint(block.blockhash(block.number-1)) % users.length;
-        // exit if only 1 user left
-        if (users.length == 1) { return; }
-        // choose a different user than the one we want to replace
-        if (newRandomU == indexU) { newRandomU = newRandomU + 1; }
-        // if we are out of the table, choose the preceding user
-        if (newRandomU > users.length) { newRandomU = newRandomU - 1; }
+        if (newRandomU == indexU) { newRandomU = newRandomU + 1; } // choose a different user than the one we want to replace
+        if (newRandomU > users.length) { newRandomU = newRandomU - 1; } // if we are out of the table, choose the preceding user
         
-        var newWinner = users[newRandomU].name;
-        //users[newRandomU].drawn = true;
         users[indexU].drawn = false; // reset previous winner
+        users[indexU].prize = ""; // and his prize
 
+        users[newRandomU].drawn = true; // draw this new user
+        users[newRandomU].prize = prizes[indexP]; // with his prize
+
+        var newWinner = users[newRandomU].name;
         ReDrawUserEvent(newRandomU, newWinner);
         return true;
     }
@@ -92,8 +93,14 @@ contract RaffleDraw {
     event AcceptDrawEvent(string winner, string prize);
 
     function AcceptDraw(uint indexU, uint indexP) onlyByAdmin() returns(bool) {
+        
+        // require(users.length > 0); // need at least 1 user to accept draw
+        // require(indexP < prizes.length && prizes.length > 0); // a prize should exist and inside the array range
+        // require(users[indexU].drawn == true); // user need to have been drawn to be able to be accepted
+        // require( sha3(users[indexU].prize) == sha3(prizes[indexP]) ); // User to accept should have been drawn with the related prize -> sha3: can only compare int/bool in require()
+
         var prize = prizes[indexP];
-        users[indexU].prize = prizes[indexP];
+        //users[indexU].prize = prizes[indexP];
         
         var winner = users[indexU].name;
         users[indexU].accepted = true;
@@ -117,4 +124,13 @@ contract RaffleDraw {
     //     return true;
     // }
 
+    // function Test1(uint indexU, uint indexP) onlyByAdmin() returns(bytes32, bytes32) {
+    //     var a = sha3(users[indexU].prize);
+    //     var b = sha3(prizes[indexP]);
+    //     require(a == b);
+    //     return (a,b);
+    // }
+
 }
+
+
